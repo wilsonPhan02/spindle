024ba3c56bb0af02721aaf3551bbbd614427fb95
bebensianipar <bebensianipar@gmail.com>
2026-07-02 14:38:53 +0700

    Fix white spots at the corners of the book cover by adjusting page layer height

--- resources/views/livewire/projects/show.blade.php
+++ resources/views/livewire/projects/show.blade.php
@@ -126,12 +126,12 @@ new #[Layout('layouts.app')] class extends Component {
                     <!-- Spine shadow for 3D effect -->
                     <div class="absolute inset-y-0 left-0 w-8 bg-gradient-to-r from-black/60 via-black/10 to-transparent z-20 pointer-events-none mix-blend-multiply rounded-l-md"></div>
                     <!-- Book pages on the right -->
-                    <div class="absolute top-[1.5%] bottom-[1.5%] right-2 w-4 bg-gradient-to-r from-[#E8E3D9] to-[#D5C6A9] border-y border-r border-[#C4B7A3] rounded-r-md z-10 shadow-inner"></div>
+                    <div class="absolute top-3 bottom-3 right-2 w-4 bg-gradient-to-r from-[#E8E3D9] to-[#D5C6A9] border-y border-r border-[#C4B7A3] rounded-r-sm z-10 shadow-inner"></div>
                     <!-- Back cover sticking out -->
                     <div class="absolute inset-y-0 right-0 w-4 bg-[#8C7558] rounded-r-lg z-0 shadow-xl border-l border-black/20"></div>
                 @else
-                    <x-default-project class="absolute inset-y-0 left-0 right-4 w-[calc(100%-16px)] h-full text-[#B69F78] rounded-l-md rounded-r-lg shadow-md z-20 border-r border-black/10" preserveAspectRatio="none" />
-                    <div class="absolute top-[1.5%] bottom-[1.5%] right-2 w-4 bg-gradient-to-r from-[#E8E3D9] to-[#D5C6A9] border-y border-r border-[#C4B7A3] rounded-r-md z-10 shadow-inner"></div>
+                    <x-default-project class="absolute inset-y-0 left-0 right-4 w-[calc(100%-16px)] h-full text-[#B69F78] rounded-l-md rounded-r-lg shadow-md z-20 border-r border-black/10" />
+                    <div class="absolute top-3 bottom-3 right-2 w-4 bg-gradient-to-r from-[#E8E3D9] to-[#D5C6A9] border-y border-r border-[#C4B7A3] rounded-r-sm z-10 shadow-inner"></div>
                     <div class="absolute inset-y-0 right-0 w-4 bg-[#8C7558] rounded-r-lg z-0 shadow-xl border-l border-black/20"></div>
                 @endif
 
