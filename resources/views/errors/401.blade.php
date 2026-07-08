8367208859c9a95cfab557d532ad43f58d71a7f4
bebensianipar <bebensianipar@gmail.com>
2026-07-08 21:29:34 +0700

    feat: Add missing standard error pages (401, 402, 429)

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,6 @@
+@extends('errors.layout')
+
+@section('title', 'Unauthorized')
+@section('code', '401')
+@section('heading', 'Unrecognized Identity')
+@section('description', 'You must reveal your true identity before entering this realm. Please authenticate yourself to proceed.')
