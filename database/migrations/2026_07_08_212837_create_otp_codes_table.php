8c439b5dc83d32ed0abae78e5f55621e3d196be5
wilsonPhan02 <wilsonphan002@gmail.com>
2026-07-08 22:08:58 +0700

    feat: add otp verification(local) without 3rd party

---
+++ database/migrations/2026_07_08_212837_create_otp_codes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('otp_codes', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('user_id');
+            $table->string('otp_code', 10);
+            $table->timestamp('expires_at');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('otp_codes');
+    }
+};
