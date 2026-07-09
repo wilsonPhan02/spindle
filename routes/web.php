1df6f314c303b1b57d016b8dd96d0415a4566cf8
bebensianipar <bebensianipar@gmail.com>
2026-07-09 08:35:04 +0700

    chore: execute phase 1 housekeeping refactor

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Pintu keluar sementara untuk testing
-Route::get('/logout-test', function () {
-    Auth::logout();
-    request()->session()->invalidate();
-    request()->session()->regenerateToken();
-    return redirect('/login');
-});
 
 Volt::route('/forgot-password', 'auth.forgot-password')->name('password.request');
 Volt::route('/reset-password/{token}', 'auth.reset-password')->name('password.reset');
